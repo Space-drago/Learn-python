b75312b47fad08d9fcd328fa0698d93ade92cbbe
Ian <79468662+Space-drago@users.noreply.github.com>
2021-03-17 10:37:34 -0700

    Update and rename README.md to main.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Learn-python
-Learn python
---
+++ main.py
@@ -0,0 +1,26 @@
+#Line comments Use # in front of your comment. When you run the program the comments will be ignored
+print("Hello world")
+#Print text. Always put the text inside "" or ''
+input("Text here: ")
+#Prints "Text here: "then the user can input text
+x = 3
+y = 'Hi'
+z = True
+#Variable: Stores a value
+print(z)
+#prints the value of "z"
+if z = true:
+  print("z is true")
+  #if statement: Runs code IF a condition is true. z = True so "z is true will be printed
+
+
+print(x and y) #1
+print(z or y) #2
+#1 : prints x AND y
+#2 : prints z OR y
+
+list = [0,1,2,3,4,5,6,7,8]
+# a list can store diffrent values
+print(list(len))
+# prints the length of "list"
+print(x(len))
